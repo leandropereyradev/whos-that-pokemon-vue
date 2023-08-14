f476a8069200a5c6d79f979be76a0c5fcb0792e9
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-14 12:06:14 +0200

    Rendering PokemonPicture & PokemonOptions Components

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: 'Pokemon Picture',
+}
+</script>
+
+<style></style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,20 @@
 <template>
   <h1>Who's That Pokemon</h1>
+
+  <PokemonPicture />
+
+  <PokemonOptions />
 </template>
 
 <script>
-export default {
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import PokemonOptions from '@/components/PokemonOptions.vue';
 
+export default {
+  name: 'Pokemon Page',
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  }
 }
 </script>
